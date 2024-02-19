1fe5bc53b176a83fd5cc4ae239b3804942f77235
Felipe-dot <felipeluis610@gmail.com>
2024-02-19 09:32:20 -0300

    Improve todo item style

--- components/todoItem.jsx
+++ components/todoItem.jsx
@@ -12,27 +12,32 @@ const TodoItem = ({ todo }) => {
   };
 
   return (
-    <div className=" p-4 cursor-pointer border-b-[1px] border-b-[--very-dark-grayish-blue-light]">
+    <div className="p-5 cursor-pointer border-b-[1px] border-b-[--dark-grayish-blue]">
       <li>
         <div className="relative">
           {isChecked && (
             <div
-              className="absolute inset-y-0 flex  items-center justify-center pl-1.5 z-10"
+              className="absolute inset-y-0 flex items-center justify-center pl-1.5 z-10"
               onClick={handleCheckBoxClick}
             >
-              <Image src={iconCheck} height={12} width={12} alt="check logo" />
+              <Image src={iconCheck} height={15} width={15} alt="check logo" />
             </div>
           )}
           <div className="flex items-center ">
             <input
-              className=" w-[1.5em] h-[1.5em] appearance-none border-[2px]  shadow-sm rounded-full cursor-pointer checked:bg-gradient-to-r from-[--gradient-color-blue] to-[--gradient-color-pink] z-0"
+              className=" w-[1.8em] h-[1.8em] appearance-none border-[2px]  shadow-sm rounded-full cursor-pointer checked:bg-gradient-to-r from-[--gradient-color-blue] to-[--gradient-color-pink] z-0"
               type="checkbox"
               id={todo.title}
               checked={isChecked}
               onChange={() => setIsChecked((prev) => !prev)}
               name={todo.title}
             />
-            <label className="px-2" htmlFor={todo.title}>
+            <label
+              className={`px-2 text-lg font-semibold text-[--very-dark-grayish-blue-light] ${
+                isChecked ? "line-through text-[--dark-grayish-blue]" : ""
+              }`}
+              htmlFor={todo.title}
+            >
               {todo.title}
             </label>
           </div>
